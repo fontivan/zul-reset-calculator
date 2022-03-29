c1bc930de1aaa180477b88e2b199ad23d303c746
Steven Skeard <stevenskeard@gmail.com>
2022-03-29 15:43:12 -0400

    Put all the addon code in a src/ folder & configure pkgmeta for automated building with CurseForge

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
+# The current directory
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-
+# The name of the addon
 ADDON_NAME="zul-reset-calculator"
+
+# The path to the WoW addon directory you want to install to
 WOW_ADDON_DIR="${1}"
 
-LOCAL_ADDON_DIR="${DIR}/${ADDON_NAME}"
+# The local directory in this repository
+LOCAL_ADDON_DIR="${DIR}/src/${ADDON_NAME}"
+
+# The addon directory inside the provided WoW addon folder
 INSTALL_DIR="${WOW_ADDON_DIR}/${ADDON_NAME}"
 
+# If the addon already exists then delete it
 if [[ -d "${INSTALL_DIR}" ]]
 then
     echo "Cleaning existing addon"
     rm -r "${INSTALL_DIR}"
 fi
 
+# Install the addon
 echo "Installing addon"
-cp -r "${DIR}/zul-reset-calculator/" "${INSTALL_DIR}"
+cp -r "${LOCAL_ADDON_DIR}" "${INSTALL_DIR}"
