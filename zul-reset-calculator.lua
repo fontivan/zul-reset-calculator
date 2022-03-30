f9940be0dee6bca86dc02c9c2076a8b23eb876fb
Steven Skeard <stevenskeard@gmail.com>
2022-03-30 14:11:27 -0400

    Implement support for sending to a specific chat channel

--- zul-reset-calculator.lua
+++ zul-reset-calculator.lua
@@ -6,16 +6,83 @@ SLASH_ZULRESETCALCULATOR3 = "/zulresetcalculator"
 
 -- Handler for processing the slash commands
 local function ZulResetCalculator_Commands(msg)
-	input = tonumber(msg)
-	if type(input) == "number" then
-		ZulResetCalculator_PrintResets(input)
-	else
-		ZulResetCalculator_PrintResets(3)
+
+	-- Parse the first two arguments out of the message
+	local _, _, arg1, arg2 = string.find(msg, "%s?(%w+)%s?(.*)")
+
+	-- By default the count will be 3
+	local count = 3
+
+	-- By default the channel will be nil
+	local channel
+
+	-- Check if the first arg is either a number or a channel
+	if type(tonumber(arg1)) == "number" then
+		count = tonumber(arg1)
+	elseif type(arg1) == "string" then
+		channel = arg1
 	end
+
+	-- Check if the second arg is either a number or a channel
+	if type(tonumber(arg2)) == "number" then
+		count = tonumber(arg2)
+	elseif type(arg2) == "string" then
+		channel = arg2
+	end
+
+	ZulResetCalculator_SendToChannel(count, channel)
+
+end
+
+function ZulResetCalculator_SendToChannel(count, channel)
+
+	-- Obj used to store channel data
+	local channels = {};
+
+	-- Say
+	channels['s'] = "SAY"
+	channels['say'] = "SAY"
+	channels['S'] = "SAY"
+	channels["SAY"] = "SAY"
+
+	-- Yell
+	channels['y'] = "YELL"
+	channels['yell'] = "YELL"
+	channels['Y'] = "YELL"
+	channels["YELL"] = "YELL"
+
+	-- Raid
+	channels['r'] = "RAID"
+	channels['raid'] = "RAID"
+	channels['R'] = "RAID"
+	channels["RAID"] = "RAID"
+	
+	-- Guild
+	channels['g'] = "GUILD"
+	channels['guild'] = "GUILD"
+	channels['G'] = "GUILD"
+	channels["GUILD"] = "GUILD"
+
+	-- Party
+	channels['p'] = "PARTY"
+	channels['party'] = "PARTY"
+	channels['P'] = "PARTY"
+	channels["PARTY"] = "PARTY"
+
+	-- Send the output to the appropriate channel
+	local text_output = ZulResetCalculator_GetResets(count)	
+	for i=1,count+1 do
+		if channel == nil or channel == "" or channels[channel] == nil then
+			print(text_output[i])
+		else
+			SendChatMessage(text_output[i], channels[channel])
+		end
+	end
+
 end
 
 -- Print the raid resets to the chat box
-function ZulResetCalculator_PrintResets(count)
+function ZulResetCalculator_GetResets(count)
 
 	-- If count is very large then we will pretend it is not to prevent problems calculating e.g. the next 2 billion Zul raid resets
 	if count > 25 then
@@ -53,16 +120,16 @@ function ZulResetCalculator_PrintResets(count)
 	local last_reset = current_unix_epoch - (time_delta % seconds_per_reset)
 
 	-- Generate the output
-	local text_output = ""
-	text_output = text_output .. "Upcoming Zul Resets:\n"
+	local text_output = {}
+	text_output[1] = "Upcoming Zul Resets:\n"
 	for i=1,count do
 		local next_date = date("%c", last_reset + i * seconds_per_reset)
 		local chat_string = string.format("Reset #%d: %s\n", i, next_date)
-		text_output = text_output .. chat_string
+		text_output[i+1] = chat_string
 	end
 
 	-- Print the output to the chat box
-	print(text_output)
+	return text_output
 end
 
 -- Add the slash commands to the client's command list
